5a2249cc9128654ad7ec949e121b2b01aea656e9
eipiequals-1 <olivier.lvannier@gmail.com>
2021-10-10 17:23:23 -0700

    working slideshow with js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+//alert("yo");
+var slideIndex = 0;
+window.addEventListener("DOMContentLoaded", slideshow);
 
-function thing(a) {
 
+function slideshow() {
+    var i;
+    var slides = document.getElementsByClassName("slideImages");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    slides[slideIndex].style.display = "block";
+    setTimeout(slideshow, 4000);
 }
